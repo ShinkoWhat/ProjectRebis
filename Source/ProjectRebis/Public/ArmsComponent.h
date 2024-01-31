22c651be1936b04e703696be6d0af512cec710ba
ShinkoWhat <platonovhuh@gmail.com>
2024-02-01 05:26:19 +1000

    Added weapon handler c++ component

---
+++ Source/ProjectRebis/Public/ArmsComponent.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ArmsComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWeaponChange);
+
+UCLASS(BlueprintType, Blueprintable)
+class PROJECTREBIS_API UArmsComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UArmsComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(BlueprintAssignable, Category="Components|Activation")
+	FOnWeaponChange WeaponChangeCallback;
+
+	UPROPERTY(EditAnywhere, Category="WeaponClass")
+	TSubclassOf<AActor> BaseWeaponClass;
+
+	//UPROPERTY(BlueprintReadWrite)
+	//TArray<BaseWeaponClass> WeaponArray;
+
+	//UPROPERTY(EditAnywhere, Category="WeaponClass")
+	//TSoftClassPtr<BaseWeaponClass> CurrentWeapon;
+};
